bb2213b9bf94abcc2eae75793388c76e0131d81b
Carlo Stearns <carlo@sigfig.com>
2015-03-06 13:17:14 -0800

    Added slim, restructured app

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,38 @@
 // gulp
 var gulp = require('gulp');
+var slim = require('gulp-slim');
 var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
 
 // plugins
 var connect = require('gulp-connect');
 
 
-gulp.task('connect', function () {
-  connect.server({
-    root: 'app/',
-    port: 8888
-  });
+gulp.task('connect', ['slim', 'sass'], function () {
+    connect.server({
+        root: 'build/',
+        port: 8888
+    });
+});
+
+gulp.task('slim', function () {
+    gulp.src('./app/views/**/*.slim')
+        .pipe(slim())
+        .pipe(gulp.dest('build'));
 });
 
 gulp.task('sass', function () {
-  gulp.src('./app/sass/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./app/.css'));
+    gulp.src('./app/styles/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./app/styles/**/*.scss', ['sass']);
+    gulp.watch('./app/views/**/*.slim', ['slim']);
+});
+
+gulp.task('default', ['connect', 'watch'], function () {
+
 });
