060ab1c9d30c87174c3d9648bf1799cd7b2aba79
Carlo Stearns <carlo.stearns@gmail.com>
2015-03-31 01:35:44 -0700

    --wip--

--- app/scripts/main.ts
+++ app/scripts/main.ts
@@ -1,3 +1,8 @@
 /// <reference path="../../typings/angularjs/angular.d.ts" />
 
 var app = angular.module('BrewCoffee', ['ngMaterial']);
+app.controller('MyController', ($scope, $mdSidenav) => {
+    $scope.openMenu = () => {
+        $mdSidenav('left').toggle();
+    }
+});
